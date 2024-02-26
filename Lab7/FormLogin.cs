b9634716efa342403496b8c74713821cf7e47614
Trevor Banhart <tbanhart11@gmail.com>
2024-02-26 15:43:53 -0800

    Added placeholder comments for Login functions

--- Lab7/FormLogin.cs
+++ Lab7/FormLogin.cs
@@ -12,9 +12,41 @@ namespace Lab7
 {
     public partial class FormLogin : Form
     {
+        string _username { get; set; }
+        string _password { get; set; }
+
+        static string _usernameColumn = string.Empty;
+        static string _passwordColumn = string.Empty;
+
         public FormLogin()
         {
             InitializeComponent();
+            _username = string.Empty;
+            _password = string.Empty;
+        }
+    
+        // Get the username and password from textboxes
+        void GetLogin()
+        {
+            // _username = TEXTUSERNAME
+            // _password = TEXTPASSWORD
+        }
+
+        // Validate email
+        void ValidateEmail()
+        {
+            // NYI try to find where the instructor buried the it in lectures
+        }
+
+        // Check login with server
+        bool VerifyLogin()
+        {
+            bool isValid = false;
+            
+            // open connection
+            // select where _usernameColumn = _username & _passwordColumn = _password
+            
+            return isValid;
         }
     }
 }
